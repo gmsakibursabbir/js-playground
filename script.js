d1cbf60ee6b15aa03a80053c92372075e23c998d
Sakibur Rahman Sabbir <106433632+gmsakibursabbir@users.noreply.github.com>
2023-09-28 20:41:28 +0600

    main

---
+++ script.js
@@ -0,0 +1,50 @@
+// button
+
+// const button = document.querySelector("button");
+
+// button.addEventListener("click", updateName);
+
+// function updateName() {
+//   const name = prompt("Enter a new name");
+//   button.textContent = `Player 1: ${name}`;
+// }
+
+
+
+// == Number ==
+
+// const myInt = 5;
+// const myFloat = 6.667;
+
+// console.log( typeof myInt);
+// console.log(typeof myFloat);
+
+
+
+
+// const lotsOfDecimal = 1.766584958675746364;
+
+// lotsOfDecimal;
+
+// const twoDecimalPlaces = lotsOfDecimal.toFixed(9);
+
+// console.log(twoDecimalPlaces);
+
+
+
+
+// let myNumber = "74";
+// myNumber = Number(myNumber) + 3;
+
+// console.log( typeof myNumber)
+
+
+const num1 = 10;
+const num2 = 50;
+
+// 9 * num1;
+// num1 ** 3;
+// const ans = num2 / num1;
+
+console.log((num2 % 9) * num1);
+console.log(num2 + num1 / 8 + 2);
